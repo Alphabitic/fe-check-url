5f1660a36cae97806ba212153bcc942c03d0d6f6
Alphabitic <alphabitic@gmail.com>
2023-03-20 08:40:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components'; 
 import { Button, CircularProgress } from '@mui/material';
-import { CancelOutlined, CheckCircleOutlineOutlined, PlayArrowRounded} from '@mui/icons-material';
+import { CancelOutlined, CheckCircleOutlineOutlined, PlayArrowRounded, WarningOutlined} from '@mui/icons-material';
 import "./index.css";
+import axios from 'axios';
+
 
 const fadeIn = keyframes `from { opacity: 0; } to { opacity: 1; }`;
 
@@ -12,35 +14,37 @@ const Link = styled.a `color: #3f51b5; text-decoration: none; font-weight: bold;
 
 
 function App() {
-const [linkStatuses, setLinkStatuses] = useState([]);
-const [loading, setLoading] = useState(false);
-const [animation, setAnimation] = useState(false);
+  const [linkStatuses, setLinkStatuses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [animation, setAnimation] = useState(false);
 
-const handleCheckLinks = async () => {
-  setLoading(true); // changer le state de loading
-  const response = await fetch('https://tes-url.onrender.com/links');
-  const data = await response.json();
-  setTimeout(() => {
-  setLoading(false);
-  setLinkStatuses(data);
-  startAnimation(); // lancer l'animation
-  }, 2000);
+
+
+  const handleCheckLinks = async () => {
+    setLoading(true); // changer le state de loading
+    const response = await fetch('https://tes-url.onrender.com/links');
+    const result = await response.json();
+    setTimeout(() => {
+      setLoading(false);
+      setLinkStatuses(result);
+      startAnimation(); // lancer l'animation
+    }, 2000);
   };
   
   function startAnimation() {
-  setAnimation(true);
-  const intervalId = setInterval(() => {
-  setAnimation(false);
-  clearInterval(intervalId);
-  }, 3000);
+    setAnimation(true);
+    const intervalId = setInterval(() => {
+      setAnimation(false);
+      clearInterval(intervalId);
+    }, 3000);
   }
-
+  
 return (
   <div className='App-header ' >
 <Container>
-  <Button onClick={handleCheckLinks} variant="contained" color="primary" startIcon={<PlayArrowRounded />}>
-    Vérifier les URLs
-  </Button>
+<Button onClick={handleCheckLinks} variant="contained" color="primary" startIcon={<PlayArrowRounded />} style={{ marginTop: '10px' }}>
+Vérifier les URLs
+</Button>
   {loading ? (
     <CircularProgress color="primary" />
   ) : (
@@ -62,8 +66,8 @@ return (
               <Link href={linkStatus.link} target="_blank" style={{ fontSize: '12pt' }}>
                 {linkStatus.link.includes('/lm_auth_proxy?') ? linkStatus.link.replace(/\/lm_auth_proxy?.*/g, '') : linkStatus.link}
               </Link>
-              {linkStatus.link === 'https://envoludia.neocles.com' && linkStatus.status === 'Lien à vérifier sur bastion' ? (
-                <span style={{ fontStyle: 'italic', color: 'gray', fontSize: '12pt' }}> - Lien à vérifier sur bastion</span>
+             {linkStatus.link === 'https://envoludia.neocles.com' && linkStatus.status === 'Cliquez sur le lien' ? (
+                <span style={{ fontStyle: 'italic', color: 'gray', fontSize: '12pt' }}> </span>
               ) : null}
             </td>
             <td style={{ border: '1px solid black', padding: '8px', fontSize: '12pt' }}>
@@ -77,16 +81,33 @@ return (
                   }}
                 />
               ) : (
-                <CancelOutlined
-                  style={{
-                    color: 'red',
-                    fontWeight: 'bold',
-                    marginRight: '8px',
-                    animation: animation ? 'crossmark 1s forwards' : '',
-                  }}
-                />
-              )}{' '}
-              {linkStatus.status}
+                <>
+                  {linkStatus.link === 'https://envoludia.neocles.com' && linkStatus.status === 'Cliquez sur le lien' ? (
+                    <>
+                      <WarningOutlined
+                        style={{ color: 'orange', fontWeight: 'bold', marginRight: '8px' }}
+                      />
+                      <span style={{ fontStyle: 'italic', color: 'gray', fontSize: '12pt' }}>
+                        - Ce lien est à vérifier sur bastion
+                      </span>
+                    </>
+                  ) : (
+                    <>
+                      <CancelOutlined
+                        style={{
+                          color: 'red',
+                          fontWeight: 'bold',
+                          marginRight: '8px',
+                          animation: animation ? 'crossmark 1s forwards' : '',
+                        }}
+                      />
+                      <span style={{ fontStyle: 'italic', color: 'gray', fontSize: '12pt' }}>
+                        "{linkStatus.status}"
+                      </span>
+                    </>
+                  )}
+                </>
+              )}
             </td>
           </tr>
         ))}
